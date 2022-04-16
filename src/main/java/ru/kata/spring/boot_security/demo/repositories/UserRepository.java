9cc02419bbe6a19243c49ea45e37d526c78b05b1
Myasoedov <myasoedov.leon@mail.ru>
2022-04-16 11:13:12 +0300

    com1

---
+++ src/main/java/ru/kata/spring/boot_security/demo/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package ru.kata.spring.boot_security.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import ru.kata.spring.boot_security.demo.models.User;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByUsername(String username);
+
+    @Modifying
+    @Query(value = "update User u set u.username = ?1, u.password = ?2 where u.id =?3")
+    void update(String username, String password,Long id);
+
+}
